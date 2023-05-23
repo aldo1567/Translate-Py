7411b86557dc24d2480b79344d81839f1bd2f3d9
aldo1567 <ronaldo@importir.co>
2023-05-23 11:04:43 +0700

    Initial Commit

---
+++ Translate.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from googletrans import Translator
+
+app = Flask(__name__)
+translator = Translator()
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    data = request.get_json()
+    text = data['text']
+    target_language = data['target_language']
+    source_language = data.get('source_language')  # Get the source language from the request payload, if provided
+
+    translated = translator.translate(text, src=source_language, dest=target_language)
+    translated_text = translated.text
+
+    response = {'translated_text': translated_text}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
